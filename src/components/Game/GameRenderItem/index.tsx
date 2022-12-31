ec831fb9c31a30d2e63840f4c4c77b86c8dc25a1
Robbert van Dommelen <robbert.van.dommelen7@gmail.com>
2022-12-31 23:51:24 +0100

    chore: add option to press a cell

--- src/components/Game/GameRenderItem/index.tsx
+++ src/components/Game/GameRenderItem/index.tsx
@@ -1,6 +1,7 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { Pressable, Text } from 'react-native';
 import { GameStateProps } from '../../../utils/game';
+import useGame from '../useGame';
 import createStyles from './styles';
 
 interface props {
@@ -12,9 +13,21 @@ function GameRenderItem(props: props) {
   const { x, y, value } = item;
 
   const styles = useMemo(() => createStyles({ x, y }), [x, y]);
+  const { setGame, game } = useGame();
+
+  const onPress = useCallback(() => {
+    const oldGameState = [...game];
+    const index = oldGameState.findIndex(
+      oldGameStateItem => oldGameStateItem.x === x && oldGameStateItem.y === y
+    );
+
+    oldGameState[index].value = 'X';
+
+    setGame(oldGameState);
+  }, [x, y, setGame, game]);
 
   return (
-    <Pressable style={styles.container}>
+    <Pressable style={styles.container} onPress={onPress}>
       <Text style={styles.text}>{value}</Text>
     </Pressable>
   );
