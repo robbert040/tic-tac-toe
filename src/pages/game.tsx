a4f1127c4ffe5d55ceadfb2c486784a7dde5106e
Robbert van Dommelen <robbert.van.dommelen7@gmail.com>
2022-12-31 20:08:00 +0100

    chore: add pages

---
+++ src/pages/game.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function game() {
+  return (
+    <View>
+      <Text>game</Text>
+    </View>
+  );
+}
+
+export default game;
